4f666144901ce867b8097597cac6225fd3f9a548
Paul K. Korir, PhD <pkorir@ebi.ac.uk>
2021-10-06 20:55:14 +0100

    some documentation

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ Here is a simple module that solves quadratic equations.
 I will use it to demonstrate how to write unit tests.
 The module tests.py has all the tests for the functions here.
 
-
 We can run this module as follows:
 
 python main.py 1 2 1
@@ -17,7 +16,12 @@ import sys
 def solve(a, b, c):
     """Solve a quadratic equation from the three coefficients
 
-    :param float
+    :param float a: coefficient of x^2
+    :param float b: coefficient of x^1
+    :param float c: coefficient of x^0
+    :return: solutions to the quadratic equation
+    :rtype: tuple(float, float)
+    :rtype: tuple(complex, complex)
     """
     discriminant = b ** 2 - 4 * a * c
     if discriminant >= 0:
@@ -35,8 +39,11 @@ def main():
         b = float(sys.argv[2])
         c = float(sys.argv[3])
     except IndexError:
-        print(f"usage: ./main.py a b c", file=sys.stderr)
+        print(f"usage: python main.py a b c", file=sys.stderr)
         return os.EX_USAGE
+    except ValueError:
+        print(f"usage: ./main.py a b c", file=sys.stderr)
+        return os.EX_DATAERR
     x1, x2 = solve(a, b, c)
     print(f"Solution for the quadratic equation {a}*x^2{b:+}*x{c:+} = 0")
     print(f"x1 = {x1}")
